66c424eb604bdab82912f58904f1526f333a7321
Eric <eglassman@gmail.com>
2012-10-08 12:32:01 -0700

    commits all unversioned files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstProject::Application.config.secret_token = 'a1778281df3ba15e48f015c496b0f1686c6e66d8057872e0096d34e194b3baee53217cc87cfbbd1eaaeb163dfde865b8b3206dac2afeb0ad15d1df1e4c0ca6d0'
